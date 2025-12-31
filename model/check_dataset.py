e976075ba54a381bd3f0ffb073962d3dca9c72bf
Aequor Wu <252141826+Aequor-Wu@users.noreply.github.com>
2025-12-31 09:50:59 +0800

    Update check_dataset.py

--- model/check_dataset.py
+++ model/check_dataset.py
@@ -1,25 +1,28 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# 修改成你自己的数据集路径
-DATASET_PATH = "D:\AAA-Bird_Species_Recognition\CUB-200-2011"
+TRAIN_PATH = "D:\AAA-Bird_Species_Recognition\dataset\CUB-200-2011"  # 改成你的真实路径
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
-dataset = datasets.ImageFolder(
-    root=DATASET_PATH,
+train_dataset = datasets.ImageFolder(
+    root=TRAIN_PATH,
     transform=transform
 )
 
-loader = DataLoader(dataset, batch_size=8, shuffle=True)
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=8,
+    shuffle=True
+)
 
-print("Number of images:", len(dataset))
-print("Number of classes:", len(dataset.classes))
-print("First 5 classes:", dataset.classes[:5])
+print("Number of training images:", len(train_dataset))
+print("Number of classes:", len(train_dataset.classes))
+print("First 5 classes:", train_dataset.classes[:5])
 
-images, labels = next(iter(loader))
+images, labels = next(iter(train_loader))
 print("Image batch shape:", images.shape)
 print("Label batch shape:", labels.shape)
